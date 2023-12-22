1fccef7221f367fbc2e9ebce721e134e77a436ea
Mustafa <secure-admin@indicatortester.com>
2023-12-22 19:19:44 +0300

    Add api key to next.config.js as AWS Amplify required

--- next.config.js
+++ next.config.js
@@ -13,6 +13,7 @@ const nextConfig = {
         X_INDICATOR_API: "https://e8bqvdcj9m.eu-west-1.awsapprunner.com",
         // X_INDICATOR_API: "http://0.0.0.0:3010", // For Debugging
         TWELVE_DATA_API: "https://api.twelvedata.com",
+        X_INDICATOR_API_KEY: process.env.X_INDICATOR_API_KEY,
     },
 };
 
