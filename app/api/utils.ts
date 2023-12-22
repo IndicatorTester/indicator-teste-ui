501f8425a58d16cd94801e7b1041947a16b79c57
Mustafa <secure-admin@indicatortester.com>
2023-12-22 18:13:09 +0300

    Fix process.env issue on production

--- app/api/utils.ts
+++ app/api/utils.ts
@@ -1,7 +1,5 @@
 import crypto from "crypto";
 
-const X_INDICATOR_API_SECRET = process.env.X_INDICATOR_API_KEY;
-
 export const hasClientServerAccess = (
     key1: string,
     key2: string,
@@ -25,7 +23,7 @@ export const hasClientServerAccess = (
 export const XIndicatorApiHeaders = () => {
     const timestamp = Date.now().toString();
 
-    const value = timestamp + X_INDICATOR_API_SECRET + timestamp;
+    const value = timestamp + process.env.X_INDICATOR_API_KEY + timestamp;
     const hash = crypto.createHash("sha512").update(value).digest("hex");
 
     return {
