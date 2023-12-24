b027d44f883ff07585f31b4c5f72febf3985e47a
Mustafa <secure-admin@indicatortester.com>
2023-12-24 08:42:59 +0300

    Remove no longer needed info alert

--- app/app/FormView.tsx
+++ app/app/FormView.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import XSelect, { Option } from "../components/XSelect";
-import { Info } from "react-feather";
 
 const INDICATOR_REGEX = /^[a-zA-Z0-9,()+\-*/|&<>=. ]+$/;
 
@@ -174,12 +173,7 @@ const FormView: React.FC<FormViewProps> = ({
                     Indicator Tester
                 </h1>
             </div>
-            <div className="w-full">
-                <div role="alert" className="alert alert-info">
-                    <Info />
-                    <span>This tool is still under development</span>
-                </div>
-            </div>
+            <div></div>
             <div className="w-full flex md:flex-row flex-col md:space-x-8 md:space-y-0 space-y-8 justify-start items-center">
                 <select
                     name="type"
