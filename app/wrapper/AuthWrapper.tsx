3743b9b20d53a4948dff33c3fe962e0c62e065b1
Mustafa <secure-admin@indicatortester.com>
2024-01-02 19:51:32 +0300

    Show fake views in case of Auth0 error

--- app/wrapper/AuthWrapper.tsx
+++ app/wrapper/AuthWrapper.tsx
@@ -3,13 +3,11 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import React, { ReactNode } from "react";
 import Loading from "../loading";
-import NotFoundPage from "../not-found";
 
 const AuthWrapper = ({ children }: { children: ReactNode }) => {
     const { user, error, isLoading } = useUser();
 
     if (isLoading) return <Loading />;
-    if (error) return <NotFoundPage />;
 
     return <>{children}</>;
 };
