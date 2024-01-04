e6123319d3536e01f149a07156d4aabc8901e88a
Mustafa <secure-admin@indicatortester.com>
2024-01-04 11:35:27 +0300

    Change pricing view and system

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,147 +1,97 @@
 "use client";
 
 import React, { useState } from "react";
+import pricingData from "@/public/static/pricing.json";
 
 const PricingPage = () => {
-    const offers = [
-        {
-            name: "Basic",
-            details: [
-                "150 tests per month",
-                "Last 10 tests history",
-                "Get last 20 transactions for each test",
-                "US market stocks",
-            ],
-            price: 29,
-        },
-        {
-            name: "Standard",
-            details: [
-                "300 tests per month",
-                "Last 25 tests history",
-                "Get last 50 transactions for each test",
-                "US market stocks",
-            ],
-            price: 39,
-        },
-        {
-            name: "Pro",
-            details: [
-                "750 tests per month",
-                "Last 50 tests history",
-                "Get last 50 transactions for each test & the ability of downloading all transactions",
-                "US market stocks",
-                "Cryptocurrencies",
-                "All available symbols on your twelvedata plan",
-            ],
-            price: 99,
-        },
-    ];
-
-    const [isMonthly, setIsMonthly] = useState(true);
-
     return (
         <>
             <div className="col-span-2"></div>
-            <div className="col-span-8 row-span-1 min-h-screen">
-                <div className="flex flex-col space-y-16 justify-start items-center text-center">
-                    <h1 className="lg:text-6xl text-5xl font-black">
-                        Simple Pricing
-                    </h1>
-                    <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-12">
-                        <div className="w-full col-span-1 lg:col-span-3 flex flex-col space-y-6 justify-center items-center">
-                            <div className="w-full flex flex-row justify-center items-center space-x-3 font-bold">
-                                <p
-                                    className={`transition-colors duration-300 ease-in-out ${
-                                        isMonthly ? "" : "text-gray-500"
-                                    }`}
-                                >
-                                    Monthly
-                                </p>
-                                <input
-                                    type="checkbox"
-                                    className="toggle"
-                                    onChange={() => setIsMonthly(!isMonthly)}
-                                />
-                                <p
-                                    className={`transition-colors duration-300 ease-in-out ${
-                                        isMonthly ? "text-gray-500" : ""
-                                    }`}
-                                >
-                                    Annual
-                                </p>
-                            </div>
-                            <div
-                                className={`transition-opacity duration-300 ease-in-out text-lg ${
-                                    isMonthly ? "opacity-0" : "opacity-100"
-                                }`}
-                            >
-                                Two months for{" "}
-                                <span className="text-xl font-bold text-green-400">
-                                    Free
-                                </span>
-                            </div>
+            <div className="col-span-8 row-span-1 min-h-screen flex justify-center">
+                <div className="max-w-[720px] flex flex-col space-y-4 justify-start">
+                    <div className="w-full bg-base-200 p-8 rounded-3xl flex flex-col space-y-8 justify-start">
+                        <h1 className="text-5xl font-bold">Pricing</h1>
+                        <div>
+                            <p className="text-gray-400">
+                                Simple & Flexible pricing system. Pay as you
+                                want to use, depending on your trading style.
+                            </p>
                         </div>
-                        {offers.map((offer, index) => {
-                            return (
-                                <div
-                                    key={index}
-                                    className="bg-neutral shadow-2xl rounded-3xl p-6 flex flex-col space-y-4 justify-start"
-                                >
-                                    <h2 className="text-xl font-black text-red-500">
-                                        {offer.name}
+                    </div>
+                    <div className="divider"></div>
+                    <div className="w-full bg-base-200 p-8 rounded-3xl flex flex-col space-y-6 justify-start">
+                        <h1 className="text-3xl font-bold">
+                            Before You Start:
+                        </h1>
+                        <div className="flex flex-col space-y-2 text-gray-400">
+                            <p>
+                                <span className="font-black text-neutral-content">
+                                    •{" "}
+                                </span>
+                                Make sure to only buy as you need, currently
+                                refund is NOT supported.
+                            </p>
+                            <p>
+                                <span className="font-black text-neutral-content">
+                                    •{" "}
+                                </span>
+                                You can have more than one type of tests.
+                            </p>
+                            <p>
+                                <span className="font-black text-neutral-content">
+                                    •{" "}
+                                </span>
+                                If you have more than one test type which is
+                                eligible for the test you run, then the cheapest
+                                test type will be used.
+                            </p>
+                        </div>
+                    </div>
+                    <div></div>
+                    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8">
+                        {pricingData.map((price, index) => (
+                            <div
+                                key={index}
+                                className="w-full bg-base-200 p-4 rounded-3xl flex flex-col space-y-8"
+                            >
+                                <div className="col-span-4 flex flex-col space-y-2">
+                                    <h1 className="text-xl font-bold col-span-4 text-center">
+                                        {price.title}
+                                    </h1>
+                                    <h2 className="text-3xl font-black text-green-400 col-span-2 text-center">
+                                        <span className="text-lg">$</span>
+                                        {price.amount}
                                     </h2>
-                                    <p className="text-4xl font-black text-green-400">
-                                        <span className="text-lg align-text-top">
-                                            $
-                                        </span>
-                                        <span>
-                                            {" "}
-                                            {offer.price * (isMonthly ? 1 : 10)}
-                                        </span>
-                                        <span className="text-lg text-neutral-content font-normal">
-                                            / {isMonthly ? "month" : "year"}
-                                        </span>
-                                    </p>
-                                    {!isMonthly && (
-                                        <div className="w-full flex justify-center items-center text-lg">
-                                            was ${offer.price * 12}
-                                        </div>
-                                    )}
-                                    <div></div>
-                                    <button className="btn btn-outline">
-                                        <span className="font-black">
-                                            Coming Soon...
-                                        </span>
-                                    </button>
-                                    <div></div>
-                                    <div className="flex flex-col space-y-2 text-start">
-                                        {offer.details.map((detail, index) => (
-                                            <p
-                                                key={index}
-                                                className="flex space-x-2"
-                                            >
-                                                <span className="font-black">
-                                                    <svg
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                        viewBox="0 0 20 20"
-                                                        fill="currentColor"
-                                                        className="w-5 h-5"
-                                                    >
-                                                        <path
-                                                            fillRule="evenodd"
-                                                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                                            clipRule="evenodd"
-                                                        />
-                                                    </svg>
-                                                </span>
-                                                <span> {detail}</span>
-                                            </p>
-                                        ))}
-                                    </div>
                                 </div>
-                            );
-                        })}
+                                <button className="col-span-4 btn btn-outline">
+                                    {price.action}
+                                </button>
+                                <div className="col-span-4 flex flex-col space-y-2">
+                                    {price.details.map((detail, index) => (
+                                        <p
+                                            key={index}
+                                            className="flex space-x-2"
+                                        >
+                                            <span className="font-black">
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                    className="w-5 h-5"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                </svg>
+                                            </span>
+                                            <span>{detail}</span>
+                                        </p>
+                                    ))}
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
